4257622c8fc442f521e1714957cd4d64fbbbd4e8
Folded-sanWasTaken <foldedsan@gmail.com>
2026-01-28 14:35:09 -0500

    Ajouté logique pour exo2.py

--- exo2.py
+++ exo2.py
@@ -19,12 +19,87 @@ Objectif :
 """
 
 FACTEURS = [1.30, 1.15, 1.05, 0.95, 0.95, 1.05, 1.15, 1.30]
+personnes = [-1, -1, -1, -1, -1, -1, -1, -1]
 
 # TODO: Lire 8 entiers (un par ligne) dans une liste personnes
 #       En cas d'erreur de conversion ou valeur negative -> afficher le message d'erreur et quitter
 
+for i in range(len(personnes)):
+    match i:
+        case 0:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section A: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 1:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section B: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 2:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section C: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 3:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section D: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 4:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section E: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 5:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section F: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case 6:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section G: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+        case _:
+            while personnes[i] < 0:
+                try:
+                    personnes[i] = int(input(f"Entrez le nombre de personnes dans la section H: "))
+                    print("Erreur - donnees invalides.") if personnes[i] < 0 else None
+                except ValueError:
+                    print("Erreur - donnees invalides.")
+
 # TODO: Calculer les intensites brutes (liste de 8 floats)
+intensites = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+for i in range(len(personnes)):
+    intensites[i] = personnes[i] * FACTEURS[i]
 
 # TODO: Calculer les niveaux normalises (liste de 8 entiers dans [0,10])
+niveaux = [0, 0, 0, 0, 0, 0, 0, 0]
+maxI = max(intensites)
+if not maxI == 0:
+     for i in range(len(personnes)):
+        niveaux[i] = int((intensites[i] / maxI) * 10 + 0.5)
 
 # TODO: Afficher la grille (10 lignes) puis la ligne des labels
+for i in range(10):
+    celllules = ""
+    for j in range(len(personnes)):
+        celllules += " ❚" if intensites[j] >= (10 - i) else " ."
+    print(f"{10 - i:2} |{celllules}")
+
+print("     A B C D E F G H")
