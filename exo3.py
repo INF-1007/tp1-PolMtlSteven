4fd6f3a44ee8098ed7879f7cef7bc33dbb6921c1
Folded-sanWasTaken <foldedsan@gmail.com>
2026-02-02 10:12:30 -0500

    Ajouté logique pour exo3.py

--- exo3.py
+++ exo3.py
@@ -27,11 +27,70 @@ Prompts EXACTS :
 """
 
 # TODO: Importer math
+import math
 
 # TODO: Lire les 4 valeurs
+distance = -1
+mins_attente_navette = -1
+mins_trajet_metro = -1
+mins_controle_entree = -1
+
+
+
+while distance < 0.0 or mins_attente_navette < 0.0 or mins_trajet_metro < 0.0 or mins_controle_entree < 0.0: # Boucler tant qu'il y des valeurs invalides
+                if distance < 0: # Lire donnée seulement si elle est invalide   
+                    try:
+                        distance = float(input("Entrez la distance jusqu'au CEPSUM (en kilometres) : "))
+                        print("Erreur - donnees invalides.") if distance < 0 else None
+                    except ValueError:
+                        print("Erreur - donnees invalides.")
+                elif mins_attente_navette < 0:
+                    try:
+                        mins_attente_navette = float(input("Entrez le temps d'attente de la navette (en minutes) : "))
+                        print("Erreur - donnees invalides.") if mins_attente_navette < 0 else None
+                    except ValueError:
+                        print("Erreur - donnees invalides.")
+                elif mins_trajet_metro < 0:
+                    try:
+                        mins_trajet_metro = float(input("Entrez le temps du trajet en metro (en minutes) : "))
+                        print("Erreur - donnees invalides.") if mins_trajet_metro < 0 else None
+                    except ValueError:
+                        print("Erreur - donnees invalides.")
+                elif mins_controle_entree < 0:
+                    try:
+                        mins_controle_entree = float(input("Entrez le temps de controle a l'entree (en minutes) : "))
+                        print("Erreur - donnees invalides.") if mins_controle_entree < 0 else None
+                    except ValueError:
+                        print("Erreur - donnees invalides.")
 
 # TODO: Validation
 
 # TODO: Calculer, arrondir (ceil) et determiner le(s) meilleur(s)
+marche  = math.ceil(distance * 60 / 5 + mins_controle_entree)
+navette = math.ceil(mins_attente_navette + distance * 60 / 18 + mins_controle_entree)
+metro   = math.ceil(mins_trajet_metro + mins_controle_entree)
+choix = [marche, navette, metro]
+
+trajet_plus_court = min(marche, navette, metro)
+chaine = ""
+
+for i in range(len(choix)):
+     match i:
+          case 0:
+               chaine += "marche " if choix[i] == trajet_plus_court else ""
+          case 1:
+               chaine += "navette " if choix[i] == trajet_plus_court else ""
+          case 2: 
+               chaine += "metro" if choix[i] == trajet_plus_court else ""
+
+options = chaine.strip().split(sep=" ")
+
 
 # TODO: Afficher la phrase exacte
+
+if len(options) == 1:
+     print(f"Option la plus rapide : {options[0]}.")
+elif len(options) == 2:
+     print(f"Egalite : {options[0]} et {options[1]}.")
+else:
+     print("Egalite : marcher, navette et metro.")
